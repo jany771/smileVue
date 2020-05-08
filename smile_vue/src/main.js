925f366fbf1aecfb11ad13e3c386fb198c9047c4
jany771 <zhjnjnjn93@163.com>
2020-05-08 23:00:19 +0800

    vant 引入

---
+++ smile_vue/src/main.js
@@ -0,0 +1,10 @@
+import Vue from 'vue'
+import App from './App.vue'
+import {Button} from 'vant'
+
+Vue.config.productionTip = false
+
+Vue.use(Button)
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
